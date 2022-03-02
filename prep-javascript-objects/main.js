440aa4fbec27da0f9cae187048c709cd5778ca97
Kyle Parrent <kppp433@gmail.com>
2022-03-02 00:54:46 +0000

    Completed prep-javascript-objects

---
+++ prep-javascript-objects/main.js
@@ -0,0 +1,13 @@
+var person = {
+  firstName: 'Paul',
+  lastName: 'Atreides',
+  hobby: 'having blue eyes'
+};
+console.log(person);
+var fullName = (person.firstName + person.lastName);
+console.log(fullName);
+person.job = 'messiah';
+console.log(person.job);
+person.previousJob = 'rich boy';
+console.log(person.previousJob);
+console.log(person);
